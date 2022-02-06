8d8b9c079c202ed4f3e32727a1ca8c08cc01bd9d
Alton <alton_ong@live.com.sg>
2022-02-06 16:36:11 +1100

    Handle checkout ready

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const App = () => {
 	// States declaration
 	const [products, setProducts] = useState([]);
 	const [cart, setCart] = useState({});
+	const [order, setOrder] = useState({});
+	const [errorMessage, setErrorMessage] = useState("");
 
 	// Fetching APIs
 	const fetchProducts = async () => {
@@ -40,6 +42,23 @@ const App = () => {
 		const { cart } = await commerce.cart.empty();
 		setCart(cart);
 	};
+
+	const refreshCart = async () => {
+		const newCart = await commerce.cart.refresh();
+		setCart(newCart);
+	};
+
+	const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+		try{
+			const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+			setOrder(incomingOrder);
+			refreshCart();
+
+		} catch (error) {
+			setErrorMessage(error.data.error.message);
+		}
+	}
+
 	useEffect(() => {
 		fetchProducts();
 		fetchCart();
@@ -66,7 +85,17 @@ const App = () => {
 						}
 					/>
 
-					<Route path="/checkout" element={<Checkout cart={cart}/>}/>
+					<Route
+						path="/checkout"
+						element={
+						<Checkout
+							cart={cart}
+							order={order}
+							onCaptureCheckout={handleCaptureCheckout}
+							error={errorMessage}
+						/>
+						}
+					/>
 				</Routes>
 
 
